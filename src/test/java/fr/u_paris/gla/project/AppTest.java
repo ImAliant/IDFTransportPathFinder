9cb502baef2c320c5c940cecdedef105b00048f6
DIAMANT alexandre <diamant.alexandre@gmail.com>
2024-03-11 15:10:04 +0100

    empty test files

--- src/test/java/fr/u_paris/gla/project/AppTest.java
+++ src/test/java/fr/u_paris/gla/project/AppTest.java
@@ -1,11 +1,32 @@
 package fr.u_paris.gla.project;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.io.File;
+import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /** Unit test for simple App. */
 class AppTest {
+    @BeforeEach
+    void setUp() {
+        // Delete the output.csv file before each test
+        File file = new File("target/output.csv");
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+
+    @AfterEach
+    void reset() {
+        App.reset();
+    }
+
     @Test
     void testLaunch() throws InterruptedException {
         App.launch();
@@ -16,5 +37,50 @@ class AppTest {
         System.out.println("testLaunch OK");
     }
 
-    
+    @Test
+    void testInitNetworkFileExists() {
+        File file = new File("target/output.csv");
+        try {
+            assertTrue(file.createNewFile());
+        } catch (Exception e) {
+            fail("Failed to create output.csv the file");
+        }
+
+        App.initNetwork();
+
+        // verify that extraction() was not called
+        assertTrue(!App.extractionCalled, "extraction() should not have been called");
+
+        // verify that load() was called
+        assertTrue(App.loadCalled, "load() should have been called");
+
+        System.out.println("testInitNetworkFileExists OK");
+    }
+
+    @Test
+    void testInitNetworkFileDoesNotExist() {
+        App.initNetwork();
+
+        // verify that extraction() was called
+        assertTrue(App.extractionCalled, "extraction() should have been called");
+
+        // verify that load() was called
+        assertTrue(App.loadCalled, "load() should have been called");
+
+        System.out.println("testInitNetworkFileDoesNotExist OK");
+    }
+
+    @Test
+    void testExtractionCalled() {
+        try {
+            App.extraction();
+        } catch (IOException e) {
+            fail("extraction() should not throw an exception");
+        }
+
+        // verify that extraction() was called
+        assertTrue(App.extractionCalled, "extraction() should have been called");
+
+        System.out.println("testExtractionCalled OK");
+    }
 }
