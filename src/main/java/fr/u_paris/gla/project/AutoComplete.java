d51aaf89b6190a952bf33d998c168dcd8d57d2a4
DIAMANT alexandre <diamant.alexandre@gmail.com>
2024-04-22 17:32:49 +0200

    rewrite show suggestions

--- src/main/java/fr/u_paris/gla/project/AutoComplete.java
+++ src/main/java/fr/u_paris/gla/project/AutoComplete.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
@@ -19,21 +20,18 @@ import fr.u_paris.gla.project.idfnetwork.Network;
 import fr.u_paris.gla.project.idfnetwork.Stop;
 
 public class AutoComplete {
+    private AutoComplete() {}
 
     public static void showSuggestions(JTextField prefix, JPanel allStations) {
         List<Stop> stops = Network.getInstance().getStops();
-        List<String> stationNames = new ArrayList<>();
-        stops.forEach(stop -> {
-            stationNames.add(stop.getStopName());
-        });
 
-        String text = prefix.getText();
         allStations.removeAll();
         Border blackline = BorderFactory.createLineBorder(Color.black);
-        for (String station : stationNames) {
-            if (station.toLowerCase().contains(text.toLowerCase())) {
+        for (Stop stop : stops) {
+            String station = stop.getStopName();
+            if (station.toLowerCase().contains(prefix.getText().toLowerCase())) {
 
-                JLabel suggestionLabel = new JLabel(station, JLabel.CENTER);
+                JLabel suggestionLabel = new JLabel(station, SwingConstants.CENTER);
 
                 suggestionLabel.setBorder(blackline);
                 suggestionLabel.addMouseListener(
@@ -49,10 +47,38 @@ public class AutoComplete {
             }
         }
         allStations.revalidate();
-        allStations.repaint();
+
+        /* List<String> stationNames = new ArrayList<>();
+        stops.forEach(stop -> stationNames.add(stop.getStopName()));
+
+        String text = prefix.getText();
+        allStations.removeAll();
+        Border blackline = BorderFactory.createLineBorder(Color.black);
+        for (String station : stationNames) {
+            if (station.toLowerCase().contains(text.toLowerCase())) {
+
+                JLabel suggestionLabel = new JLabel(station, SwingConstants.CENTER);
+
+                suggestionLabel.setBorder(blackline);
+                suggestionLabel.addMouseListener(
+                        new MouseAdapter() {
+                            @Override
+                            public void mouseClicked(MouseEvent e) {
+                                prefix.setText(station);
+                                allStations.removeAll();
+
+                            }
+                        });
+                allStations.add(suggestionLabel);
+            }
+        }
+        allStations.revalidate();
+        allStations.repaint(); */
     }
 
     public static DocumentListener myDocumentListener(JTextField t, JPanel j) {
+        // TODO Create a listener to show suggestions when the text changes
+
         DocumentListener res =
                 new DocumentListener() {
                     @Override
