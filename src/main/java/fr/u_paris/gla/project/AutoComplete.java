4fd6b50f6bc6edc2659198b09d12b7cdc08f345f
ghizlane <ghizlaneaziz32@gmail.zom>
2024-04-25 19:36:29 +0200

    improve  GUI

--- src/main/java/fr/u_paris/gla/project/AutoComplete.java
+++ src/main/java/fr/u_paris/gla/project/AutoComplete.java
@@ -1,29 +1,32 @@
 package fr.u_paris.gla.project;
+
 import javax.swing.border.Border;
 
 import java.awt.Color;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.List;
-import javax.swing.JPanel;
 import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
-
 
 import fr.u_paris.gla.project.idfnetwork.Network;
 import fr.u_paris.gla.project.idfnetwork.stop.Stop;
 import fr.u_paris.gla.project.idfnetwork.view.CustomTextField;
-import fr.u_paris.gla.project.idfnetwork.view.SuggestionStationsScrollPane;
+import fr.u_paris.gla.project.idfnetwork.view.SuggestionStationsComboBox;
+
 
 public class AutoComplete {
-    private AutoComplete() {}
+    private static Network instance = Network.getInstance();
 
-    public static void showSuggestions(CustomTextField prefix, SuggestionStationsScrollPane suggestions) {
-        List<Stop> stops = Network.getInstance().getStops();
-        JPanel allStations = suggestions.getSuggestions();
+    private AutoComplete() {
+    }
 
-        allStations.removeAll();
+    public static void showSuggestions(CustomTextField prefix, SuggestionStationsComboBox suggestions) {
+        List<Stop> stops = instance.getStops();
+
+        suggestions.clearSuggestion();
         Border blackline = BorderFactory.createLineBorder(Color.black);
         for (Stop stop : stops) {
             String station = stop.getStopName();
@@ -35,18 +38,18 @@ public class AutoComplete {
 
                 suggestionLabel.setBorder(blackline);
                 suggestionLabel.addMouseListener(
-                        new MouseAdapter() {
-                            @Override
-                            public void mouseClicked(MouseEvent e) {
-                                prefix.setText(stopWithLines);
-                                allStations.removeAll();
-
-                            }
-                        });
-                allStations.add(suggestionLabel);
+                    new MouseAdapter() {
+                        @Override
+                        public void mouseClicked(MouseEvent e) {
+                            prefix.setText(station);
+                            
+                            SwingUtilities.invokeLater(() -> suggestions.clearSuggestion());
+                        }
+                    }
+                );
+                suggestions.addItem(stopWithLines);
             }
         }
-        allStations.revalidate();
-        allStations.repaint();
     }
+
 }
