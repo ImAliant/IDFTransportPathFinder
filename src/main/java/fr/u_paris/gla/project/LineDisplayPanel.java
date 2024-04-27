b1915761f4c85a2fa12fe684bd8b2b27f58058c1
DIAMANT alexandre <diamant.alexandre@gmail.com>
2024-04-27 13:30:54 +0200

    #41 ready to be writed

--- src/main/java/fr/u_paris/gla/project/LineDisplayPanel.java
+++ src/main/java/fr/u_paris/gla/project/LineDisplayPanel.java
@@ -2,6 +2,14 @@ package fr.u_paris.gla.project;
 
 import javax.swing.JPanel;
 
-public class LineDisplayPanel extends JPanel {
+import fr.u_paris.gla.project.observer.LineDisplayPanelObserver;
+
+public class LineDisplayPanel extends JPanel implements LineDisplayPanelObserver {
+
+    @Override
+    public void updateVisibility() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateVisibility'");
+    }
     
 }
--- src/main/java/fr/u_paris/gla/project/idfnetwork/view/button/OpenLineDisplayPanelButton.java
+++ src/main/java/fr/u_paris/gla/project/idfnetwork/view/button/OpenLineDisplayPanelButton.java
@@ -1,23 +1,35 @@
 package fr.u_paris.gla.project.idfnetwork.view.button;
 
-import javax.swing.Icon;
-import javax.swing.SwingUtilities;
+import java.util.List;
+import java.util.ArrayList;
 
-import fr.u_paris.gla.project.LineDisplayPanel;
-import fr.u_paris.gla.project.utils.IconUtils;
+import fr.u_paris.gla.project.observer.LineDisplayPanelObserver;
 
 
 public class OpenLineDisplayPanelButton extends MiniButton {
+    private List<LineDisplayPanelObserver> observers = new ArrayList<>();
+
     private static final String PATH = "";
 
-    public OpenLineDisplayPanelButton(int x, int y/* , LineDisplayPanel panel */) {
+    public OpenLineDisplayPanelButton(int x, int y) {
         super(x, y);
 
         /* Icon icon = IconUtils.createIcon(PATH, WIDTH, HEIGHT);
         setIcon(icon); */
+    }
 
-        addActionListener(e -> /* SwingUtilities.invokeLater(() ->
-            panel.setVisible(!panel.isVisible())
-        ) */ System.out.println("OpenLineDisplayPanelButton"));
+    public void addObserver(LineDisplayPanelObserver observer) {
+        observers.add(observer);
+    }
+
+    private void notifyObservers() {
+        observers.forEach(LineDisplayPanelObserver::updateVisibility);
+    }
+
+    @Override
+    public void onClick() {
+        System.out.println("OpenLineDisplayPanelButton.onClick");
+
+        /* notifyObservers(); */
     }
 }
