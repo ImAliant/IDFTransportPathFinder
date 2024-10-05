49abdf1ec693d3231517a26f90e35fa3651f40ad
ImAliant <111398111+ImAliant@users.noreply.github.com>
2024-10-05 18:35:25 +0200

    comment for the command folder

--- src/main/java/fr/u_paris/gla/crazytrip/command/GuiCommand.java
+++ src/main/java/fr/u_paris/gla/crazytrip/command/GuiCommand.java
@@ -12,21 +12,41 @@ import fr.u_paris.gla.crazytrip.utils.ApplicationUtils;
 import fr.u_paris.gla.crazytrip.utils.CommandUtils;
 import fr.u_paris.gla.crazytrip.utils.NetworkBackendHandler;
 
+/**
+ * This class represents a GUI command.
+ * It is used to start the application with a graphical user interface.
+ */
 public class GuiCommand implements Command {
+	/**
+	 * Start the application with a graphical user interface.
+	 * 
+	 * @param args The arguments of the command. 
+	 * 			   It can contain the argument "noextract" to avoid the extraction of the network data.
+	 * 			   And the argument "--console" to start the application with a console interface. 
+	 */
     @Override
     public void execute(String[] args) {
+		// Load the application properties
         Properties props = ApplicationUtils.readApplicationProperties();
         String title = props.getProperty("app.name");
 
+		// Start the loading screen
         SwingUtilities.invokeLater(LoadingScreen.getInstance()::start);
-
+		// Check if the extraction should be done
         checkExtraction(args);
 
         Network.getInstance();
 
+		// Start the user interface
         checkInterface(args, title);
     }
 
+	/**
+	 * Check if the extraction should be done. 
+	 * 
+	 * @param args Can contain the an argument "noextract" to avoid the extraction 
+	 * 			   of the network data.
+	 */
     private static void checkExtraction(String[] args) {
 		boolean extract = true;
 		for (String arg: args) {
@@ -44,6 +64,14 @@ public class GuiCommand implements Command {
 		}
 	}
 
+	/**
+	 * <br>Start the user interface.</br>
+	 * <br>Two types of interfaces can be used: ONLINE or CONSOLE.</br>
+	 * <br>The default interface is ONLINE.</br>
+	 * 
+	 * @param args The arguments of the command. If the argument "--console" is present, the interface will be CONSOLE.
+	 * @param title The title of the application
+	 */
 	private static void checkInterface(String[] args, String title) {
 		String type = "ONLINE";
 		for (String arg: args) {
