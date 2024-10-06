1616783bf320742edcf7ceae70fdb8f0bc13cded
ImAliant <111398111+ImAliant@users.noreply.github.com>
2024-10-06 20:59:32 +0200

    comment for the listener folder

--- src/main/java/fr/u_paris/gla/crazytrip/gui/listener/LocationTextFieldDocumentListener.java
+++ src/main/java/fr/u_paris/gla/crazytrip/gui/listener/LocationTextFieldDocumentListener.java
@@ -7,15 +7,39 @@ import javax.swing.event.DocumentListener;
 import fr.u_paris.gla.crazytrip.gui.combobox.SuggestionComboBox;
 import fr.u_paris.gla.crazytrip.gui.textfield.LocationTextField;
 
+/**
+ * Class representing a listener for the location text field.
+ * 
+ * @see DocumentListener
+ * @see LocationTextField
+ */
 public class LocationTextFieldDocumentListener implements DocumentListener {
+    /** The location text field */
     private LocationTextField textField;
+    /** The suggestion combo box */
     private SuggestionComboBox suggestionComboBox;
 
+    /**
+     * Constructor.
+     * 
+     * @param textField the location text field
+     * @param suggestionComboBox the suggestion combo box
+     * 
+     * @see LocationTextField
+     * @see SuggestionComboBox
+     */
     public LocationTextFieldDocumentListener(LocationTextField textField, SuggestionComboBox suggestionComboBox) {
         this.textField = textField;
         this.suggestionComboBox = suggestionComboBox;
     }
 
+    /**
+     * <p>Inserts an update.</p>
+     * 
+     * <p>When the text field is updated, the suggestion combo box is displayed and the suggestions are updated.</p>
+     * 
+     * @param e the document event
+     */
     @Override
     public void insertUpdate(DocumentEvent e) {
         if (!suggestionComboBox.isVisible()) {
@@ -25,16 +49,33 @@ public class LocationTextFieldDocumentListener implements DocumentListener {
         checkAndUpdateSuggestions();
     }
 
+    /**
+     * <p>Changes an update.</p>
+     * 
+     * <p>When the text field is updated, the suggestion combo box is displayed and the suggestions are updated.</p>
+     * 
+     * @param e the document event
+     */
     @Override
     public void changedUpdate(DocumentEvent arg0) {
         checkAndUpdateSuggestions();
     }
 
+    /**
+     * <p>Removes an update.</p>
+     * 
+     * <p>When the text field is updated, the suggestion combo box is displayed and the suggestions are updated.</p>
+     * 
+     * @param e the document event
+     */
     @Override
     public void removeUpdate(DocumentEvent arg0) {
         checkAndUpdateSuggestions();
     }
     
+    /**
+     * Check if the text field is empty and update the suggestions.
+     */
     private void checkAndUpdateSuggestions() {
         SwingUtilities.invokeLater(() -> {
             if (textField.getText().isEmpty()) {
--- src/main/java/fr/u_paris/gla/crazytrip/gui/listener/StyleButtonListener.java
+++ src/main/java/fr/u_paris/gla/crazytrip/gui/listener/StyleButtonListener.java
@@ -6,11 +6,28 @@ import java.awt.event.MouseEvent;
 
 import fr.u_paris.gla.crazytrip.gui.button.StyleButton;
 
+/**
+ * Listener for a style button.
+ * 
+ * @see StyleButton
+ */
 public class StyleButtonListener extends MouseAdapter {
+    /** The button. */
     private final StyleButton button;
+    /** The default color of the button. */
     private final Color defaultColor;
+    /** The color of the button when the mouse is over it. */
     private final Color hoverColor;
 
+    /**
+     * Constructor.
+     * 
+     * @param button the button
+     * @param defaultColor the default color of the button
+     * @param hoverColor the color of the button when the mouse is over it
+     * 
+     * @see StyleButton
+     */
     public StyleButtonListener(StyleButton button, Color defaultColor, Color hoverColor) {
         this.button = button;
         this.defaultColor = defaultColor;
@@ -22,11 +39,17 @@ public class StyleButtonListener extends MouseAdapter {
         button.action();
     }
 
+    /**
+     * Change the background color of the button when the mouse enters it.
+     */
     @Override
     public void mouseEntered(MouseEvent e) {
         button.setBackground(hoverColor);
     }
 
+    /**
+     * Change the background color of the button when the mouse exits it.
+     */
     @Override
     public void mouseExited(MouseEvent e) {
         button.setBackground(defaultColor);
