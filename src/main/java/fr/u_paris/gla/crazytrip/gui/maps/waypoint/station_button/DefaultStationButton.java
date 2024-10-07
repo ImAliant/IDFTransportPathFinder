453a3304c589cce866f0117d6b0e498e9358895e
Alexandre DIAMANT <111398111+ImAliant@users.noreply.github.com>
2024-10-08 00:06:44 +0200

    comment for the maps/waypoint/station_button folder

--- src/main/java/fr/u_paris/gla/crazytrip/gui/maps/waypoint/station_button/DefaultStationButton.java
+++ src/main/java/fr/u_paris/gla/crazytrip/gui/maps/waypoint/station_button/DefaultStationButton.java
@@ -2,12 +2,26 @@ package fr.u_paris.gla.crazytrip.gui.maps.waypoint.station_button;
 
 import fr.u_paris.gla.crazytrip.model.Station;
 
+/**
+ * Class representing a default station button.
+ * 
+ * <p>It is used to display a station on the map.</p>
+ * 
+ * @see StationButton
+ */
 public class DefaultStationButton extends StationButton {
     /** Default path to the icon. */
     private static final String DEFAULT_PATH = "src/main/resources/stop_logo/stop_logo.png";
-
+    /** Zoom threshold for the default station button. */
     private static final int ZOOM_THRESHOLD = 0;
 
+    /**
+     * Constructor for the default station button.
+     * 
+     * @param station The station to represent.
+     * 
+     * @see Station
+     */
     public DefaultStationButton(Station station) {
         super(station, DEFAULT_PATH);
     }
--- src/main/java/fr/u_paris/gla/crazytrip/gui/maps/waypoint/station_button/MetroStationButton.java
+++ src/main/java/fr/u_paris/gla/crazytrip/gui/maps/waypoint/station_button/MetroStationButton.java
@@ -2,10 +2,26 @@ package fr.u_paris.gla.crazytrip.gui.maps.waypoint.station_button;
 
 import fr.u_paris.gla.crazytrip.model.Station;
 
+/**
+ * Class representing a metro station button on the map.
+ * 
+ * <p>It is used to display a metro station on the map.</p>
+ * 
+ * @see StationButton
+ */
 public class MetroStationButton extends StationButton {
+    /** Path to the metro icon. */
     private static final String METRO_ICON = "src/main/resources/stop_logo/metro_logo.png";
+    /** Zoom threshold for the metro station button. */
     private static final int ZOOM_THRESHOLD = 6;
 
+    /**
+     * Constructor for the metro station button.
+     * 
+     * @param station The station to represent.
+     * 
+     * @see Station
+     */
     protected MetroStationButton(Station station) {
         super(station, METRO_ICON);
     }
