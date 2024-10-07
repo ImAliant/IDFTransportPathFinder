2068c1beff06da0a75ebce869ca7a75faf81d6bb
Alexandre DIAMANT <111398111+ImAliant@users.noreply.github.com>
2024-10-08 00:32:21 +0200

    comment for the gui/observer folder

--- src/main/java/fr/u_paris/gla/crazytrip/gui/observer/PanelObserver.java
+++ src/main/java/fr/u_paris/gla/crazytrip/gui/observer/PanelObserver.java
@@ -1,5 +1,11 @@
 package fr.u_paris.gla.crazytrip.gui.observer;
 
+/**
+ * This interface is used to update the visibility of a panel.
+ */
 public interface PanelObserver {
+    /**
+     * Update the visibility of the panel.
+     */
     void updateVisibility();
 }
--- src/main/java/fr/u_paris/gla/crazytrip/gui/observer/ZoomOutObserver.java
+++ src/main/java/fr/u_paris/gla/crazytrip/gui/observer/ZoomOutObserver.java
@@ -1,5 +1,11 @@
 package fr.u_paris.gla.crazytrip.gui.observer;
 
+/**
+ * Observer used to zoom out the map
+ */
 public interface ZoomOutObserver {
+    /**
+     * Zoom out the map
+     */
     void zoomOut();
 }
