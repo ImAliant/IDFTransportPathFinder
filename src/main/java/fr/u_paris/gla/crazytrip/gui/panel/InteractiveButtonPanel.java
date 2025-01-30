b7770e203a9da05eda5b463c7c87b087da8d1078
Alexandre DIAMANT <111398111+ImAliant@users.noreply.github.com>
2025-01-30 13:56:09 +0100

    comment for the gui/panel folder

--- src/main/java/fr/u_paris/gla/crazytrip/gui/panel/InteractiveButtonPanel.java
+++ src/main/java/fr/u_paris/gla/crazytrip/gui/panel/InteractiveButtonPanel.java
@@ -10,6 +10,16 @@ import fr.u_paris.gla.crazytrip.gui.button.OpenResearchPanelButton;
 import fr.u_paris.gla.crazytrip.gui.button.ZoomInButton;
 import fr.u_paris.gla.crazytrip.gui.button.ZoomOutButton;
 
+/**
+ * Panel containing the interactive buttons.
+ * 
+ * The user will be able to open the research panel, the line selection panel and zoom in or out on the map.
+ * 
+ * @see OpenResearchPanelButton
+ * @see OpenLineSelectionPanelButton
+ * @see ZoomInButton
+ * @see ZoomOutButton
+ */
 public class InteractiveButtonPanel extends JPanel {
     /** Background color of the panel. */
     private static final Color BACKGROUND_COLOR = new Color(1, 121, 111);
@@ -22,39 +32,75 @@ public class InteractiveButtonPanel extends JPanel {
     /** Zoom out button. */
     private ZoomOutButton zoomOut;
 
+    /*
+     * Constructor
+     */
     public InteractiveButtonPanel() {
         super();
 
         setLayout(new FlowLayout(FlowLayout.LEFT));
         setBackground(BACKGROUND_COLOR);
 
+        initComponents();
+        addComponents();
+    }
+
+    /** Initialize the components of the panel */
+    private void initComponents() {
         openResearchButton = new OpenResearchPanelButton();
         openLineSelectionButton = new OpenLineSelectionPanelButton();
         zoomIn = new ZoomInButton();
         zoomOut = new ZoomOutButton();
-
-        initComponents();
     }
 
-    private void initComponents() {
+    /** Add components in the panel */
+    private void addComponents() {
         add(openResearchButton);
         add(openLineSelectionButton);
         add(zoomIn);
         add(zoomOut);
     }
 
+    /**
+     * Getter research button
+     * 
+     * @return The research button
+     * 
+     * @see OpenResearchPanelButton
+     */
     public OpenResearchPanelButton getOpenResearchButton() {
         return openResearchButton;
     }
 
+    /**
+     * Getter line selection button
+     * 
+     * @return The line selection button
+     * 
+     * @see OpenLineSelectionPanelButton
+     */
     public OpenLineSelectionPanelButton getOpenLineButton() {
         return openLineSelectionButton;
     }
 
+    /**
+     * Getter zoom in button
+     * 
+     * @return The zoom in button
+     * 
+     * @see ZoomInButton
+     */
     public ZoomInButton getZoomIn() {
         return zoomIn;
     }
 
+    /**
+     * Getter zoom out button
+     * 
+     * @return The zoom out button
+     * 
+     * @see ZoomOutButton
+     */
     public ZoomOutButton getZoomOut() {
         return zoomOut;
     }
--- src/main/java/fr/u_paris/gla/crazytrip/gui/panel/LineSelectionPanel.java
+++ src/main/java/fr/u_paris/gla/crazytrip/gui/panel/LineSelectionPanel.java
@@ -10,14 +10,32 @@ import fr.u_paris.gla.crazytrip.gui.observer.DisplayLineObserver;
 import fr.u_paris.gla.crazytrip.gui.observer.LinePainterObserver;
 import fr.u_paris.gla.crazytrip.gui.observer.PanelObserver;
 
+/**
+ * Panel where the user can choose to show all lines of the idf network on the map.
+ * 
+ * There are three components on this panel:
+ * <ol>
+ *  <li>Panel to select a type of transportation</li>
+ *  <li>Panel to select the line to show</li>
+ *  <li>Panel to clear the map</li>
+ * </ol>
+ * 
+ * @see JPanel
+ * @see PanelObserver
+ */
 public class LineSelectionPanel extends JPanel implements PanelObserver {
     /** Background color of the panel. */
     private static final Color BACKGROUND_COLOR = new Color(1, 121, 121);
-
+    /** Panel with all the transport modes */
     private TransportSelectionPanel transportSelectionPanel;
+    /** Panel to select the line */
     private SelectionPanel selectionPanel;
+    /** Panel to clear the map */
     private ClearPanel clearPanel;
 
+    /**
+     * Constructor
+     */
     public LineSelectionPanel() {
         super();
 
@@ -29,12 +47,14 @@ public class LineSelectionPanel extends JPanel implements PanelObserver {
         addComponents();
     }
 
+    /** Initialize the components */
     private void initComponents() {
         transportSelectionPanel = new TransportSelectionPanel();
         selectionPanel = new SelectionPanel();
         clearPanel = new ClearPanel();
     }
 
+    /** Add the components */
     private void addComponents() {
         add(transportSelectionPanel, BorderLayout.WEST);
         add(selectionPanel, BorderLayout.CENTER);
@@ -46,26 +66,68 @@ public class LineSelectionPanel extends JPanel implements PanelObserver {
         setVisible(!isVisible());
     }
 
+    /** 
+     * Add observer to the transport selection panel 
+     * 
+     * @param observer the observer to add. The observer will know the current transportation mode selected.
+     * 
+     * @see DisplayLineObserver 
+     */
     public void addTransportButtonsObservers(DisplayLineObserver observer) {
         transportSelectionPanel.addObservers(observer);
     }
 
+    /**
+     * Add observer to the clear panel
+     * 
+     * @param observer the observer to add. The observer will know if the map need to be cleared.
+     * 
+     * @see ClearLineObserver
+     */
     public void addClearButtonObserver(ClearLineObserver observer) {
         clearPanel.addObserver(observer);
     }
 
+    /**
+     * Add observer to the selection panel
+     * 
+     * @param observer the observer to add. The observer will know the selected line to paint.
+     * 
+     * @see LinePainterObserver
+     */
     public void addLinePainterObserver(LinePainterObserver observer) {
         selectionPanel.addLinePainterObserver(observer);
     }
 
+    /**
+     * Getter for the selection panel
+     * 
+     * @return the selection panel
+     * 
+     * @see SelectionPanel
+     */
     public SelectionPanel getSelectionPanel() {
         return selectionPanel;
     }
 
+    /**
+     * Getter for the transport selection panel
+     * 
+     * @return the transport selection panel
+     * 
+     * @see TransportSelectionPanel
+     */
     public TransportSelectionPanel getTransportSelectionPanel() {
         return transportSelectionPanel;
     }
 
+    /**
+     * Getter for the clear panel
+     * 
+     * @return the clear panel
+     * 
+     * @see ClearPanel
+     */
     public ClearPanel getClearPanel() {
         return clearPanel;
     }
