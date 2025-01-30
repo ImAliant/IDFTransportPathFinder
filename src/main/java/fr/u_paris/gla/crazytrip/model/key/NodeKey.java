209eda3d3e18491504e7e863e36d2e12ef198616
Alexandre DIAMANT <111398111+ImAliant@users.noreply.github.com>
2025-01-30 15:36:46 +0100

    comment for the model/key forlder

--- src/main/java/fr/u_paris/gla/crazytrip/model/key/NodeKey.java
+++ src/main/java/fr/u_paris/gla/crazytrip/model/key/NodeKey.java
@@ -2,10 +2,19 @@ package fr.u_paris.gla.crazytrip.model.key;
 
 import java.util.Objects;
 
+/** Key class to identify a node */
 public class NodeKey {
+    /** The name of the node */
     private final String nodeName;
+    /** The key of the line */
     private final LineKey lineKey;
     
+    /**
+     * Create a node key
+     * 
+     * @param nodeName the name of the node
+     * @param lineKey the key of the line
+     */
     public NodeKey(final String nodeName, final LineKey lineKey) {
         this.nodeName = nodeName;
         this.lineKey = lineKey;
