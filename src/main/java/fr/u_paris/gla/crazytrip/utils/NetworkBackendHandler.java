441e8475eae228b748650489fd6c37f352c72e76
Alexandre DIAMANT <111398111+ImAliant@users.noreply.github.com>
2025-01-30 14:45:40 +0100

    comment for the utils folder

--- src/main/java/fr/u_paris/gla/crazytrip/utils/NetworkBackendHandler.java
+++ src/main/java/fr/u_paris/gla/crazytrip/utils/NetworkBackendHandler.java
@@ -4,9 +4,17 @@ import java.io.IOException;
 
 import fr.u_paris.gla.crazytrip.idfm.IDFMNetworkExtractor;
 
+/**
+ * Utility class to handle network backend
+ */
 public class NetworkBackendHandler {
+    /** Prevent instantiation */
     private NetworkBackendHandler() {}
 
+    /**
+     * Extract network data
+     * @throws IOException if an I/O error occurs
+     */
     public static void extraction() throws IOException {
         IDFMNetworkExtractor.extract();
     }
